a47164a002829d1aca0058c41f9362013eec7b26
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-29 16:02:55 -0600

    initialize registrant class with name, age, permit, and license data

---
+++ lib/registrant.rb
@@ -0,0 +1,11 @@
+class Registrant
+
+    attr_reader :name, :age, :license_data, :written, :license, :renewed, :permit
+  
+    def initialize(registrant_info = {})
+      @name = registrant_info[:name]
+      @age = registrant_info[:age]
+      @license_data = registrant_info[:license_data] || {written: false, license: false, renewed: false}
+      @permit = registrant_info[:permit] = false
+    end
+end
