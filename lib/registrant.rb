473c12bbdedddf0c4bfd3bb917ba8ed86ca0a9e1
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-29 19:55:59 -0600

    add permit? and earn permit methods

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -6,6 +6,16 @@ class Registrant
       @name = registrant_info[:name]
       @age = registrant_info[:age]
       @license_data = registrant_info[:license_data] || {written: false, license: false, renewed: false}
-      @permit = registrant_info[:permit] = false
+      @permit = registrant_info[:permit]
     end
+
+    def permit?
+        @permit
+    end
+
+
+    def earn_permit
+        @permit = true
+    end
+
 end
