8e892efbba597ba757d7d408ae1e2dbab9d1fdd2
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-02 12:01:22 -0600

    add create vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,11 +1,18 @@
+require 'faraday'
+require 'json'
+
 class VehicleFactory
-
-    def initialize
-
+    def self.create_vehicles(wa_ev_registrations)
+        wa_ev_registrations.map do |vehicle|
+            {
+                engine_type: vehicle[:ev],
+                make: vehicle[:make],
+                model: vehicle[:model],
+                plate_type: vehicle[:plate_type],
+                registration_date: vehicle[:registration_date],
+                vin: vehicle[:vin],
+                year: vehicle[:year]
+            }
+        end
     end
-
-    def create_vehicles(data)
-
-    end
-
 end
