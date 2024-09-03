1fa82feb55148d2428f5d17d2eba8204be48a6b2
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-09-03 07:08:20 -0700

    Taking Out Comments

--- lib/VehicleFactory.rb
+++ lib/VehicleFactory.rb
@@ -6,8 +6,8 @@ class VehicleFactory
       data.each do |vehicle_data|
         vehicle_data.default = 'nil'
         vehicle = Vehicle.new({
-          :vin => vehicle_data[:vin],  # Use the full VIN field if available
-          :year => vehicle_data[:year],  # Use model_year as provided in the dataset
+          :vin => vehicle_data[:vin],  
+          :year => vehicle_data[:year],  
           :make => vehicle_data[:make],
           :model => vehicle_data[:model],
           :engine => :ev
--- spec/Vehicle_Factory_Spec.rb
+++ spec/Vehicle_Factory_Spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe 'VehicleFactory' do
     before(:each) do
       @factory = VehicleFactory.new
-      #@wa_ev_registrations = @dds.wa_ev_registrations
       @data = DmvDataService.new.wa_ev_registrations
     end
 it 'can initialize VehicleFactory' do
