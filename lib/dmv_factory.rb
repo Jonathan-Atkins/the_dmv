f9c0eba043158a77423b8399c24fea1c9bfd5a16
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-02 20:23:40 -0600

    add methods and tests for NY and MO facilities

--- lib/dmv_factory.rb
+++ lib/dmv_factory.rb
@@ -2,33 +2,76 @@ require 'faraday'
 require 'json'
 
 class DmvFactory
-    def create_facilities(co_dmv_office_locations)
-        co_dmv_office_locations.map do |facility_data|
-            facility = Facility.new(
-                name: facility_data[:dmv_office],
-                address: [
-                        facility_data[:address_li],
-                        facility_data[:address__1],
-                        facility_data[:city],
-                        facility_data[:state],
-                        facility_data[:zip]
-                        ].compact.join(', '),
-                phone: facility_data[:phone],
-                registered_vehicles: facility_data[:registered_vehicles],
-                collected_fees: facility_data[:collected_fees]
-            )
-            add_service(facility, facility_data[:services_p])
+    def create_co_facilities(co_dmv_office_locations)
+    co_dmv_office_locations.map do |facility_data|
+        facility = Facility.new(
+        name: facility_data[:dmv_office],
+        address: [
+                facility_data[:address_li],
+                facility_data[:address__1],
+                facility_data[:city],
+                facility_data[:state],
+                facility_data[:zip]
+                ].compact.join(', '),
+        phone: facility_data[:phone],
+        registered_vehicles: facility_data[:registered_vehicles],
+        collected_fees: facility_data[:collected_fees])
+    
+        add_service(facility, facility_data[:services_p])
 
+        facility 
+        end
+    end
+
+    def create_ny_facilities(ny_dmv_office_locations)
+    ny_dmv_office_locations.map do |facility_data|
+        facility = Facility.new(
+        name: [
+            facility_data[:office_name],
+            facility_data[:office_type],
+            ].compact.join(', '),
+        address: [
+            facility_data[:street_address_line_1],
+            facility_data[:city],
+            facility_data[:state],
+            facility_data[:zip_code],
+            ].compact.join(', '),
+        phone: facility_data[:public_phone_number],
+        registered_vehicles: facility_data[:registered_vehicles],
+        collected_fees: facility_data[:collected_fees])
+    
+        add_service(facility, facility_data[:services_p])
+
+        facility 
+        end
+        end
+
+    def create_mo_facilities(mo_dmv_office_locations)
+        mo_dmv_office_locations.map do |facility_data|
+            facility = Facility.new(
+            name: facility_data[:name],
+            address: [
+                facility_data[:address1],
+                facility_data[:city],
+                facility_data[:state],
+                facility_data[:zipcode],
+                ].compact.join(', '),
+            phone: facility_data[:phone],
+            registered_vehicles: facility_data[:registered_vehicles],
+            collected_fees: facility_data[:collected_fees])
+        
+            add_service(facility, facility_data[:services_p])
+    
             facility 
-                    end
-                end
+            end
+            end
 
     def add_service(facility, services_string)
         return unless services_string
 
         services = services_string.split(',').map(&:strip)
         services.each do |service|
-            facility.add_service(service)
+        facility.add_service(service)
         end
     end
 end
--- spec/dmv_factory_spec.rb
+++ spec/dmv_factory_spec.rb
@@ -3,24 +3,50 @@ require 'spec_helper'
 RSpec.describe DmvFactory do
     before(:each) do
         @dmv_factory = DmvFactory.new 
-        @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
     end
 
     it 'maps facility data to new array' do
-        facilities = @dmv_factory.create_facilities(@co_dmv_office_locations)
+        @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+        facilities = @dmv_factory.create_co_facilities(@co_dmv_office_locations)
 
         expect(facilities).to all(be_a(Facility))
         expect(facilities).to be_an(Array)
     end
 
-    it 'maps the correct data to the new Facility object' do
-        facilities = @dmv_factory.create_facilities(@co_dmv_office_locations)
+    it 'maps the correct data to the new Colorado Facility objects' do
+        @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+        facilities = @dmv_factory.create_co_facilities(@co_dmv_office_locations)
 
-        expect(facilities.first.name).to eq(@co_dmv_office_locations.first[:dmv_office])
+        expect(facilities.first.name).to eq("DMV Tremont Branch")
         expect(facilities.first.address).to eq("2855 Tremont Place, Suite 118, Denver, CO, 80205")
-        expect(facilities.first.phone).to eq(@co_dmv_office_locations.first[:phone])
+        expect(facilities.first.phone).to eq("(720) 865-4600")
         expect(facilities.first.services).to eq(["vehicle titles", "registration", "renewals;  VIN inspections"])
         expect(facilities.first.registered_vehicles).to eq([])
         expect(facilities.first.collected_fees).to eq(0)
     end
+
+    it 'maps the correct data to the new New York Facility objects' do
+        @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
+        facilities = @dmv_factory.create_ny_facilities(@ny_dmv_office_locations)
+
+        expect(facilities.first.name).to eq("HUNTINGTON, DISTRICT OFFICE")
+        expect(facilities.first.address).to eq("1815 E JERICHO TURNPIKE, HUNTINGTON, NY, 11743")
+        expect(facilities.first.phone).to eq("7184774820")
+        expect(facilities.first.services).to eq([])
+        expect(facilities.first.registered_vehicles).to eq([])
+        expect(facilities.first.collected_fees).to eq(0)
+    end
+
+    it 'maps the correct data to the new Missouri Facility objects' do
+        @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
+        facilities = @dmv_factory.create_mo_facilities(@mo_dmv_office_locations)
+
+        expect(facilities.first.name).to eq("OAKVILLE")
+        expect(facilities.first.address).to eq("3164 TELEGRAPH ROAD, ST LOUIS, MO, 63125")
+        expect(facilities.first.phone).to eq("(314) 887-1050")
+        expect(facilities.first.services).to eq([])
+        expect(facilities.first.registered_vehicles).to eq([])
+        expect(facilities.first.collected_fees).to eq(0)
+    end
+
 end
